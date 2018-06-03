d01e0927a52d7a684eee382139c3aabb40f87c83
Malcolm Jones <malcolm@adobe.com>
2018-06-03 14:08:19 -0400

    fix: encryption

--- encrypt_all.sh
+++ encrypt_all.sh
@@ -4,3 +4,8 @@ for f in $(find ./group_vars -name *.vault -print)
 do
   ansible-vault encrypt --vault-password-file=./vault_password $f
 done
+
+for f in $(find ./vars -name *.vault -print)
+do
+  ansible-vault encrypt --vault-password-file=./vault_password $f
+done
